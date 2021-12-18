ed0d58216ad6704f789e551dddb8f76366f7fb84
ahmedmohamedzain <ahmed.zaineldin@vodafone.com>
2021-12-18 16:30:14 +0200

    map vs switchmap

--- KotlinMVVM/app/src/main/java/com/emedinaa/kotlinmvvm/model/MuseumDbRepository.kt
+++ KotlinMVVM/app/src/main/java/com/emedinaa/kotlinmvvm/model/MuseumDbRepository.kt
@@ -1,6 +1,7 @@
 package com.emedinaa.kotlinmvvm.model
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import com.emedinaa.kotlinmvvm.data.db.DbDataSource
 import com.emedinaa.kotlinmvvm.data.db.MuseumDTO
@@ -9,15 +10,20 @@ class MuseumDbRepository(private val dataSource: DbDataSource) {
 
     fun getMuseums(): LiveData<List<Museum>> {
         val museumsFromDbDataSourceLiveData: LiveData<List<MuseumDTO>> = dataSource.museums()
-        return Transformations.map(museumsFromDbDataSourceLiveData){
-            it.map{itItem ->Museum(itItem.id,itItem.name, itItem.photo)}
+        //converts from LiveData<List<MuseumDTO>> to LiveData<List<Museum>>
+        return Transformations.map(museumsFromDbDataSourceLiveData) {
+            it.map { itItem -> Museum(itItem.id, itItem.name, itItem.photo) }//here is List<Museum>
+        }
+        //converts from LiveData<List<MuseumDTO>> to LiveData<List<Museum>>
+        return Transformations.switchMap(museumsFromDbDataSourceLiveData) {
+            MutableLiveData<List<Museum>>()//here is LiveData<List<Museum>>
         }
     }
 
-    suspend fun sync(museumList:List<Museum>){
+    suspend fun sync(museumList: List<Museum>) {
         dataSource.deleteMuseums()
         dataSource.addMuseums(museumList.map {
-            MuseumDTO(it.id,it.name,it.photo)
+            MuseumDTO(it.id, it.name, it.photo)
         })
     }
 }
